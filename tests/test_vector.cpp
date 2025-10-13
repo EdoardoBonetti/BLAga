d3b78d3cd406f54e680226915940cf7d99cb7a83
Edoardo Bonetti <bonettiedo@gmail.com>
2025-10-13 10:27:44 +0200

    split the tests in sum and mult

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -10,18 +10,15 @@ using complex = std::complex<double>;
 
 using namespace bla_ga;
 
-template <typename Tscal, typename T, typename Scal, typename S>
-void test_vector_multaddmult()
+template <typename T, typename S>
+void test_vector_add()
 {
     int n = 100;
 
     Vector<T> v1(n);
     Vector<S> v2(n);
 
-    Tscal alpha1 = 3.;
-    Scal alpha2 = 5.;
-
-    using U = decltype(Tscal{} * T{} + Scal{} * S{});
+    using U = decltype(T{} + S{});
 
     std::valarray<U> w(n);
 
@@ -30,10 +27,37 @@ void test_vector_multaddmult()
         v1(i) = T(i);      // cast i to T (works for int, double, complex)
         v2(i) = S(10 * i); // cast 10*i to S
 
-        w[i] = alpha1 * T(i) + alpha2 * S(10 * i); // cast loop variables to match scalar types
+        w[i] = T(i) + S(10 * i); // cast loop variables to match scalar types
     }
 
-    Vector<U> vout = alpha1 * v1 + alpha2 * v2;
+    Vector<U> vout = v1 + v2;
+
+    for (int i = 0; i < n; i++)
+    {
+        REQUIRE(vout(i) == w[i]);
+    }
+}
+
+template <typename T, typename S>
+void test_vector_mult()
+{
+    int n = 100;
+
+    T a = T(2);
+    Vector<S> v(n);
+
+    using U = decltype(T{} * S{});
+
+    std::valarray<U> w(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        v(i) = T(i); // cast i to T (works for int, double, complex)
+
+        w[i] = a * T(i); // cast loop variables to match scalar types
+    }
+
+    Vector<U> vout = a * v;
 
     for (int i = 0; i < n; i++)
     {
@@ -43,12 +67,18 @@ void test_vector_multaddmult()
 
 TEST_CASE("Vector addition works for multiple types", "[math]")
 {
-    SECTION("4int") { test_vector_multaddmult<int, int, int, int>(); }
-    SECTION("4double") { test_vector_multaddmult<double, double, double, double>(); }
-    SECTION("4complex") { test_vector_multaddmult<complex, complex, complex, complex>(); }
-    SECTION("2double2int") { test_vector_multaddmult<double, double, int, int>(); }
-    SECTION("2complex2double") { test_vector_multaddmult<complex, complex, double, double>(); }
-    SECTION("2double2complex") { test_vector_multaddmult<double, double, complex, complex>(); }
+    SECTION("4int") { test_vector_add<int, int>(); }
+    SECTION("4double") { test_vector_add<double, double>(); }
+    SECTION("4complex") { test_vector_add<complex, complex>(); }
+    SECTION("2double2int") { test_vector_add<double, int>(); }
+    SECTION("2complex2double") { test_vector_add<complex, double>(); }
+    SECTION("2double2complex") { test_vector_add<double, complex>(); }
+}
 
-    /*Other couples are not possible/reasonable */
+TEST_CASE("Vector multiplication works for multiple types", "[math]")
+{
+    SECTION("4int") { test_vector_mult<int, int>(); }
+    SECTION("4double") { test_vector_mult<double, double>(); }
+    SECTION("4complex") { test_vector_mult<complex, complex>(); }
+    SECTION("2double2int") { test_vector_mult<double, int>(); }
 }
