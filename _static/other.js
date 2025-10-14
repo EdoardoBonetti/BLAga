86560544a923fe9bceca3348f3b040acca907d5d
Edoardo Bonetti <bonettiedo@gmail.com>
2025-10-14 19:08:54 +0200

    Update documentation

---
+++ _static/other.js
@@ -0,0 +1,5 @@
+document.addEventListener('mousemove', e => {
+  const x = e.clientX / window.innerWidth - 0.5;
+  const y = e.clientY / window.innerHeight - 0.5;
+  document.body.style.backgroundPosition = `${50 + x*10}% ${50 + y*10}%`;
+});
