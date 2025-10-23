0796f6af18b0a9b381d674459a7d6cb3aa92c15f
Edoardo Bonetti <bonettiedo@gmail.com>
2025-10-23 15:41:13 +0200

    put by default on release

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from skbuild import setup
 
 import sys
-_cmake_args = []
+#_cmake_args = []
+_cmake_args = ['-DCMAKE_BUILD_TYPE=Release']  # <-- add this
 
 if 'win32' in sys.platform:
     _cmake_args += ['-G', 'MinGW Makefiles' ]
